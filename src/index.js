3ebfabe35a5cbd588d7771eeb052f1fe2fc47010
Kyle Crichton <kylecrichton@KYLE-CRICHTON-MBP.local>
2017-10-15 17:23:18 -0500

    added constructor to search_bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB9rz7b2_q_8Lww-4v-ZiUg5ED4WF3-I1M';
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
